db0d4719012ed5a6e48d44ded4223b1e406fa590
ajs94 <ajs94@students.calvin.edu>
2019-05-15 19:01:34 -0400

    prettying directories

--- Assets/Scripts/RoomTypes/Room.cs
+++ Assets/Scripts/RoomTypes/Room.cs
@@ -17,9 +17,6 @@ public class Room
     public Direction entranceDir;
     public Direction[] exitDirs;
 
-    /*  Vector3 exitPos = new Vector3(-1, (room.rows / 2) + Random.Range(-1, 1), 0f) + room.vectorOffset;
-        room.roomExits[3] = exitPos; */
-
     public List<Vector3> roomExits = new List<Vector3>();
 
     public void SetupRoom(int maxWidth, int maxHeight, Vector3 startPos)
